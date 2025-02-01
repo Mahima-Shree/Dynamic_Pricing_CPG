775bea16c95c149a40ab27e208b37d072e5e55b6
Mahima-Shree <87635323+Mahima-Shree@users.noreply.github.com>
2025-02-01 22:36:25 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ st.markdown("### Enter competitor pricing and discount details:")
 
 # Collect user input
 input_features = []
-feature_names = ['RevenuePerUnit', 'NoPromoPrice', 'FunnelDrop_CartToCheckout', 'AvgSessionDuration_sec', 'ReturningVisitorRatio', 'Revenue_Without_Discount', 'DiscountRate_Surf Excel', 'PriceDifference', 'CTR', 'DiscountImpact']
+feature_names = ['SellingPrice', 'NoPromoPrice', 'Revenue_Without_Discount', 'DiscountImpact', 'Revenue_With_Discount', 'UnitsSold', 'RevenuePerUnit']
 
 for feature in feature_names:
     value = st.number_input(f"{feature}:", min_value=0.0, format="%.2f")
